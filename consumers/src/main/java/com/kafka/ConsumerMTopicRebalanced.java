902c8fc606f29c84a3b5c3b3e0ad1fbfa0c599d6
wlsgur073 <wlsgur073@gmail.com>
2025-01-30 18:01:21 +0900

    컨슈머에서 여러 개의 토픽을 읽어드려야 한다. 왜냐햐면 컨슈머의 많은 역할이 topic 데이터를 읽어서 처리하는 역할을 하기에 여러 개의 토픽에 있는 데이터를 함께 가공해서 그것을 다른 데이터 소스에 넣는다거나, 필터링 하는 등의 역할을 수행해야 하기 때문이다.

---
+++ consumers/src/main/java/com/kafka/ConsumerMTopicRebalanced.java
@@ -0,0 +1,57 @@
+package com.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerMTopicRebalanced {
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerMTopicRebalanced.class);
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092"); // VM address
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-mtopic");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(List.of("topic-p3-t1", "topic-p3-t2")); // two topics
+
+        Thread mainThread = Thread.currentThread();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutdown hook");
+            consumer.wakeup(); // poll() 메서드가 블록되어 있는 상태에서 빠져나와서 InterruptedException을 발생시킴.
+
+            try {
+                mainThread.join(); // main thread가 종료될 때까지 대기
+            } catch (InterruptedException e) {
+                e.printStackTrace(); // for test
+            }
+        }));
+
+        try {
+            while (true) {
+                consumer.poll(Duration.ofMillis(1000))
+                        .forEach(record -> {
+                            logger.info("topic: {}, key: {}, partition: {},  offset: {}, value: {}"
+                                    , record.topic(), record.key(), record.partition(), record.offset(), record.value());
+                        });
+            }
+        } catch (WakeupException e) {
+            logger.error("WakeupException occurred");
+        } finally {
+            logger.info("Closing consumer");
+            consumer.close();
+        }
+
+    }
+}
